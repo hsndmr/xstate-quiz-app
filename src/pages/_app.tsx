e7e6384b9b8b7eb8920324f32b45ea66bd265618
Hasan Demir <hsndmr5044@hotmail.com>
2023-02-15 02:33:43 +0300

    Add `ThemeProvider` to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import { Plus_Jakarta_Sans } from '@next/font/google';
 
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        plusJakartaSans.style.fontFamily,
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+      ].join(','),
+    },
+    palette: {
+      background: {
+        default: grey[200],
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
